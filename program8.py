3d6b63c436a2ce165c328ed8b2af29c8be920428
nuruddin-99 <nuruddinalways99@gmail.com>
2022-11-17 21:05:10 +0600

    Program 8 Done:

---
+++ program8.py
@@ -0,0 +1,11 @@
+#Print list in reverse order using a loop
+#list1 = [10, 20, 30, 40, 50]
+#Outpur = [50, 40, 30, 20, 10]
+
+
+list = [10, 20, 30, 40, 50]
+
+new_list = reversed(list)
+
+for item in new_list :
+    print(item)
